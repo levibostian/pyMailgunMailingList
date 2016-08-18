ed285ae30edca74c140ad69fb2fd5f26f63f010a
Levi Bostian <levi.bostian@gmail.com>
2016-08-18 09:33:34 -0500

    Fix email body capture

--- send_email.py
+++ send_email.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
+
 import requests
 
 configFileLine = 0
 
 apiKey = ""
+domain = ""
 emailFrom = ""
 emailTo = ""
 
@@ -12,12 +15,14 @@ for line in open("config.txt", 'r'):
     if configFileLine == 0:
         apiKey = line[1].strip()
     elif configFileLine == 1:
-        emailFrom += line[1].strip() + " "
+        domain = line[1].strip()
     elif configFileLine == 2:
         emailFrom += line[1].strip() + " "
     elif configFileLine == 3:
-        emailFrom += "<" + line[1].strip() + ">"
+        emailFrom += line[1].strip() + " "
     elif configFileLine == 4:
+        emailFrom += "<" + line[1].strip() + ">"
+    elif configFileLine == 5:
         emailTo = line[1].strip()
 
     configFileLine += 1
@@ -31,11 +36,19 @@ for line in open("email_text.txt", "r"):
     if emailTextLine == 0:
         line = line.split(":")
         subject = line[1].strip()
-    elif emailTextLine == 4:
+    elif emailTextLine >= 4:
         body += line
 
     emailTextLine += 1
 
+print body 
 data = {"from": emailFrom, "to": emailTo, "subject": subject, "text": body}
 
-requests.post("https://api.mailgun.net/v3/curiosityio.com/messages", auth=("api", apiKey), data=data)
+
+r = requests.post("https://api.mailgun.net/v3/" + domain + "/messages", auth=("api", apiKey), data=data)
+
+if r.status_code == 200:
+    print "SUCCESS! Email queued."
+else:
+    print "Error. Didn’t work for some reason. " + r.text
+
